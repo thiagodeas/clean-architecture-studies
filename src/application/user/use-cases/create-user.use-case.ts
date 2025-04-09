0e1b273750d427ce2de29f79c7dfc8c2cc69ba2a
thiagodeas <sousaalvesth@gmail.com>
2025-04-09 18:38:41 -0300

    feat: implement password hashing

--- src/application/user/use-cases/create-user.use-case.ts
+++ src/application/user/use-cases/create-user.use-case.ts
@@ -2,6 +2,7 @@ import { UserRepository } from "src/domain/repositories/user.repository";
 import { CreateUserDto } from "../../../infra/dto/create-user.dto";
 import { User } from "src/domain/entities/user.entity";
 import { ConflictException, Inject, Injectable } from "@nestjs/common";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class CreateUserUseCase {
@@ -13,10 +14,12 @@ export class CreateUserUseCase {
             throw new ConflictException('User already exists');
         }
 
+        const hashedPassword = await bcrypt.hash(dto.password, 10);
+
         const user = new User(
             dto.name,
             dto.email,
-            dto.password,
+            hashedPassword,
         );
 
         await this.userRepository.create(user);
